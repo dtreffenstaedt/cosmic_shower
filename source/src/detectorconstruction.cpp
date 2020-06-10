173a087cae0935f59a6cc96a4810b1c1b5c6dccd
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-06-10 13:42:29 +0200

    discarded unnecessary arrays and switched to temporary local variables

--- source/src/detectorconstruction.cpp
+++ source/src/detectorconstruction.cpp
@@ -27,7 +27,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
     // +++ Construct the world
 
-    G4Box* world_box = new G4Box("World", world_length, world_length, world_length);
+    G4Box* world_box = new G4Box("World", world_side * 0.5, world_side * 0.5, world_side * 0.5);
 
     G4double atomicNumber = 1.;
     G4double massOfMole = 1.008*g/mole;
@@ -64,12 +64,13 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     // It consists of 12 layers of varying thickness.
     // The specific thickness of each layer has been numerically calculated.
     
-    G4double layers_meters[12] = {707.612*m, 1480.13*m, 2330.68*m, 3276.84*m, 4342.89*m, 5563.75*m, 6992.27*m, 8714.17,*m 10883*m, 13818.6*m, 18422.3*m, 40000*m}; // upper limit of each layer
+    G4double layers_meters[12] = {707.612*m, 1480.13*m, 2330.68*m, 3276.84*m, 4342.89*m, 5563.75*m, 6992.27*m, 8714.17*m, 10883*m, 13818.6*m, 18422.3*m, 40000*m}; // upper limit of each layer
     
     G4double layers_factor[12] = {0.958913,0.878348,0.797762,0.717148,0.636496,0.555787,0.474994,0.394064,0.312884,0.231178,0.147671,0.0312708}; // factors for the density of each layer
-    G4Material* material_layers[12]; // materials for each layer
-    G4Solid* box_layer[12]; // geometry of each layer
-    G4LogicalVolume* logical_layer[12]; // logical volumes for each layer
+//    G4Material* material_layers[12]; // materials for each layer
+//    G4VSolid* box_layer[12]; // geometry of each layer
+//    G4LogicalVolume* logical_layer[12]; // logical volumes for each layer
+    G4VPhysicalVolume* physical_layer[12]; // physical volumes for each layer
 
     
 
@@ -78,17 +79,17 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
         G4double layer_lower = ((i == 0)?(0.0*m):(layers_meters[i - 1])); // lower end of each layer
         G4double layer_thickness = layers_meters[i] - layer_lower; // layer thickness
 
-        material_layers[i] = new G4Material("material_layer" + std::to_string(i), rho_0 * layers_factor[i], air);
-        box_layer[i] = new G4Box("box_layer" + std::to_string(i), world_side * 0.5, world_side * 0.5, layer_thickness * 0.5);
-        logical_layer[i] = new G4LogicalVolume(box_layer[i], material_layers[i], std::to_string(i));
-        physical_layer[i] = new G4VPlacement(
+        G4Material* layer_material = new G4Material("material_layer" + std::to_string(i), rho_0 * layers_factor[i], air);
+        G4Box* layer_box = new G4Box("box_layer" + std::to_string(i), world_side * 0.5, world_side * 0.5, layer_thickness * 0.5);
+        G4LogicalVolume* layer_logical = new G4LogicalVolume(layer_box, layer_material, std::to_string(i));
+        physical_layer[i] = new G4PVPlacement(
                 0,
                 G4ThreeVector(
                     0.0 * m,
                     0.0 * m,
                     - world_side * 0.5 + (layer_lower + layer_thickness * 0.5) // center point of each layer 
                     ),
-                logical_layer[i],
+                layer_logical,
                 "phys_layer" + std::to_string(i),
                 logical_world,
                 false,
