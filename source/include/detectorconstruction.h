7e216f6f5b4e608425e00189c34428373b2ecfa6
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-06-13 20:15:54 +0200

    split detector construction into multiple functions to increase readability and logic separation

--- source/include/detectorconstruction.h
+++ source/include/detectorconstruction.h
@@ -2,22 +2,27 @@
 #define DETECTORCONSTRUCTION_H
 
 #include <G4VUserDetectorConstruction.hh>
+
 #include <globals.hh>
 
 #include <variant>
 
 #include "global.h"
 #include "configmanager.h"
+#include <G4SystemOfUnits.hh>
 
 class G4VPhysicalVolume;
-class G4VLogicalVolume;
+class G4LogicalVolume;
 
 START_NAMESPACE
 {
 class DetectorConstruction : public G4VUserDetectorConstruction
 {
 public:
-    DetectorConstruction(const std::variant<std::vector<Config::DetectorPlacement>, size_t>& detectors, const std::vector<Config::AtmosphereLayer>& atmosphere_layers);
+    DetectorConstruction(
+                const std::variant<std::vector<Config::DetectorPlacement>, size_t>& detectors,
+                const std::vector<Config::AtmosphereLayer>& atmosphere_layers,
+                const double& world_size = 40000);
 
     virtual ~DetectorConstruction();
 
@@ -26,6 +31,16 @@ public:
 protected:
     std::variant<std::vector<Config::DetectorPlacement>, size_t> m_detectors;
     std::vector<Config::AtmosphereLayer> m_atmosphere_layers;
+    G4double m_world_size;
+
+    G4LogicalVolume* m_world_logical;
+
+    G4VPhysicalVolume* construct_world();
+    void construct_atmosphere();
+    void construct_magnetic_field();
+    void construct_detectors();
+
+    void place_detector(const std::string &name, const G4double& x, const G4double& y, const G4double &z = 0 * m);
 };
 }
 
--- source/src/detectorconstruction.cpp
+++ source/src/detectorconstruction.cpp
@@ -9,10 +9,12 @@
 
 START_NAMESPACE
 {
-DetectorConstruction::DetectorConstruction(const std::variant<std::vector<Config::DetectorPlacement>, size_t>& detectors, const std::vector<Config::AtmosphereLayer>& atmosphere_layers) :
+DetectorConstruction::DetectorConstruction(const std::variant<std::vector<Config::DetectorPlacement>, size_t>& detectors, const std::vector<Config::AtmosphereLayer>& atmosphere_layers, const double& world_size) :
     G4VUserDetectorConstruction{},
     m_detectors{detectors},
-    m_atmosphere_layers{atmosphere_layers}
+    m_atmosphere_layers{atmosphere_layers},
+    m_world_size{world_size * m},
+    m_world_logical{nullptr}
 {}
 
 DetectorConstruction::~DetectorConstruction()
@@ -20,16 +22,24 @@ DetectorConstruction::~DetectorConstruction()
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
+    G4VPhysicalVolume* physical_world = construct_world();
 
-    G4NistManager* nist = G4NistManager::Instance();
-    G4double world_side = 40 * km; // cube shaped world, side length
-    G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
+    construct_atmosphere();
 
+    construct_magnetic_field();
+
+    construct_detectors();
+
+    return physical_world;
+}
+
+G4VPhysicalVolume* DetectorConstruction::construct_world()
+{
     // +++ Construct the world
 
-    G4Box* world_box = new G4Box("World", world_side * 0.5, world_side * 0.5, world_side * 0.5);
+    G4Box* world_box = new G4Box("World", m_world_size * 0.5, m_world_size * 0.5, m_world_size * 0.5);
 
-    G4Material* vacuum = new G4Material(
+    G4Material* vacuum = new G4Material( // world is a vacuum
             "interGalactic",
             1.,
             1.008 * g / mole,
@@ -39,12 +49,12 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
             3.e-18 * pascal);
 
 
-    G4LogicalVolume* logical_world = new G4LogicalVolume(world_box, vacuum, "World");
+    m_world_logical = new G4LogicalVolume(world_box, vacuum, "World");
 
     G4VPhysicalVolume* physical_world = new G4PVPlacement(
             0,
             G4ThreeVector(),
-            logical_world,
+            m_world_logical,
             "World",
             0,
             false,
@@ -53,14 +63,25 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
     // --- Construct the world
 
+    return physical_world;
+}
+
+void DetectorConstruction::construct_atmosphere()
+{
+    if (!m_world_logical)
+    {
+        return;
+    }
+    G4NistManager* nist = G4NistManager::Instance();
+    G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
 
     // +++ construct the atmostphere
     // It consists of 12 layers of varying thickness.
     // The specific thickness of each layer has been numerically calculated.
-    
+
     std::vector<G4VPhysicalVolume*> physical_layers; // physical volumes for each layer
 
-    
+
 
     size_t len = m_atmosphere_layers.size();
     for (size_t i = 0; i < len; i++)
@@ -73,18 +94,18 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
         G4double temperature = definition.temperature * kelvin;
 
         G4Material* layer_material = new G4Material("material_layer" + std::to_string(i), density, air, kStateUndefined, temperature, pressure);
-        G4Box* layer_box = new G4Box("box_layer" + std::to_string(i), world_side * 0.5, world_side * 0.5, layer_thickness * 0.5);
+        G4Box* layer_box = new G4Box("box_layer" + std::to_string(i), m_world_size * 0.5, m_world_size * 0.5, layer_thickness * 0.5);
         G4LogicalVolume* layer_logical = new G4LogicalVolume(layer_box, layer_material, std::to_string(i));
         physical_layers.push_back(new G4PVPlacement(
                 0,
                 G4ThreeVector(
                     0.0 * m,
                     0.0 * m,
-                    - world_side * 0.5 + (layer_lower + layer_thickness * 0.5) // center point of each layer 
+                    - m_world_size * 0.5 + (layer_lower + layer_thickness * 0.5) // center point of each layer
                     ),
                 layer_logical,
                 "phys_layer" + std::to_string(i),
-                logical_world,
+                m_world_logical,
                 false,
                 0,
                 true
@@ -92,8 +113,68 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     }
 
     // --- construct the atmostphere
+}
 
+void DetectorConstruction::construct_magnetic_field()
+{
+    if (!m_world_logical)
+    {
+        return;
+    }
+}
 
-    return physical_world;
+void DetectorConstruction::construct_detectors()
+{
+    if (!m_world_logical)
+    {
+        return;
+    }
+    std::vector<Config::DetectorPlacement> detectors;
+    if (std::holds_alternative<size_t>(m_detectors))
+    {
+        size_t n = std::get<size_t>(m_detectors);
+        double root = std::sqrt(static_cast<double>(n));
+        if (std::abs(root - std::floor(root)) <= std::numeric_limits<double>::epsilon()) // perfect square, create a uniform pattern.
+        {
+            G4double distance = m_world_size / G4double((root + 1));
+            size_t row = std::sqrt(n);
+            G4double pos_x = 0 * m;
+            for (size_t i = 0; i < row; i++)
+            {
+                pos_x += distance;
+                G4double pos_y = 0 * m;
+                for (size_t j = 0; j < row; j++)
+                {
+                    pos_y += distance;
+                    place_detector("detector" + std::to_string(i*j), pos_x, pos_y);
+                }
+            }
+        }
+        else // not a square, place detectors on circle
+        {
+            double angle = std::atan(1.0)*8/static_cast<double>(n);
+            G4double r = m_world_size * 0.25;
+            for (size_t i = 1; i <= n; i++)
+            {
+                G4double x = std::cos(angle * static_cast<double>(i)) * r + m_world_size * 0.5;
+                G4double y = std::sin(angle * static_cast<double>(i)) * r + m_world_size * 0.5;
+                place_detector("detector" + std::to_string(i), x, y);
+            }
+        }
+    }
+    else
+    {
+        detectors = std::get<std::vector<Config::DetectorPlacement>>(m_detectors);
+        for (size_t i = 0; i < detectors.size(); i++)
+        {
+            Config::DetectorPlacement placement = detectors[i];
+            place_detector(placement.name, (placement.x * m) + m_world_size * 0.5, (placement.y * m) + m_world_size * 0.5, placement.z * m);
+        }
+    }
+}
+
+void DetectorConstruction::place_detector(const std::string&, const G4double&, const G4double&, const G4double&)
+{
+
 }
 }
