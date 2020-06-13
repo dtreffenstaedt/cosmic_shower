c1bf9b5816207c98b7007c8fb98bed5fc883dc4c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-06-13 18:02:25 +0200

    Slight restructure of directory

--- source/cmake/files.cmake
+++ source/cmake/files.cmake
@@ -7,4 +7,12 @@ set(SIM_SOURCE_FILES "${PROJECT_SOURCE_DIR}/main.cpp"
                  "${PROJECT_SOURCE_DIR}/actions/steppingaction.cpp"
                  "${PROJECT_SOURCE_DIR}/configmanager.cpp"
 )
+set(SIM_HEADER_FILES "${PROJECT_INCLUDE_DIR}/detectorconstruction.h"
+                 "${PROJECT_INCLUDE_DIR}/actioninitialization.h"
+                 "${PROJECT_INCLUDE_DIR}/actions/runaction.h"
+                 "${PROJECT_INCLUDE_DIR}/actions/primarygeneratoraction.h"
+                 "${PROJECT_INCLUDE_DIR}/actions/eventaction.h"
+                 "${PROJECT_INCLUDE_DIR}/actions/steppingaction.h"
+                 "${PROJECT_INCLUDE_DIR}/configmanager.h"
+)
 set(ATMOS_SOURCE_FILES "${PROJECT_SOURCE_DIR}/atmosphere_layers.cpp")
