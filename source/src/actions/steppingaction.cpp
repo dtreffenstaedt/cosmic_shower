510086f791d47ec11dfba4097a020dd954f75cbf
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-07-25 07:45:25 +0200

    Slightly modified the stepping action to hopefully be a little bit more performant

--- source/src/actions/steppingaction.cpp
+++ source/src/actions/steppingaction.cpp
@@ -20,26 +20,25 @@ SteppingAction::~SteppingAction()
 
 void SteppingAction::UserSteppingAction(const G4Step* step)
 {
-
-    if (step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() == "Intensitycatcher") {
-        G4Track* track = step->GetTrack();
-        RecorderManager::singleton()->store_momentum(step->GetPreStepPoint()->GetPosition(), track->GetParticleDefinition()->GetPDGMass(), track->GetKineticEnergy(), (track->GetParticleDefinition()->GetPDGCharge()) > std::numeric_limits<double>::epsilon() );
+    G4Track* track = step->GetTrack();
+    const G4ParticleDefinition* particle = track->GetParticleDefinition();
+    const G4StepPoint* pre = step->GetPreStepPoint();
+    if (pre->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() == "Intensitycatcher") {
+        RecorderManager::singleton()->store_momentum(pre->GetPosition(), particle->GetPDGMass(), track->GetKineticEnergy(), (particle->GetPDGCharge()) > std::numeric_limits<double>::epsilon() );
         track->SetTrackStatus(G4TrackStatus::fStopAndKill);
         return;
     }
-    if (RecorderManager::singleton()->stored_primary()) {
-        return;
-    }
-    G4Track* track = step->GetTrack();
-    if (track->GetParentID() == 0) // only check first secondary particle
-    {
-        return;
-    }
-    if ((track->GetParticleDefinition()->GetPDGMass() + track->GetKineticEnergy()) < m_limit) {
-        return;
-    }
+    if (!RecorderManager::singleton()->stored_primary()) {
+        if (track->GetParentID() == 0) // only check first secondary particle
+        {
+            return;
+        }
+        if ((particle->GetPDGMass() + track->GetKineticEnergy()) < m_limit) {
+            return;
+        }
 
-    std::cout << "Storing initial interaction\n";
-    RecorderManager::singleton()->store_primary(step->GetPreStepPoint()->GetPosition(), step->GetPreStepPoint()->GetGlobalTime());
+        std::cout << "Storing initial interaction\n";
+        RecorderManager::singleton()->store_primary(pre->GetPosition(), pre->GetGlobalTime());
+    }
 }
 }
