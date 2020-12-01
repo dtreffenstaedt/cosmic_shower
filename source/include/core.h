529b2bc517e421bb73594b1cba6f9d0ec21b99ea
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-01 08:01:43 +0100

    Removed code for using the Geant4 GUI

--- source/include/core.h
+++ source/include/core.h
@@ -5,23 +5,10 @@
 
 #include <string>
 
-#ifdef SHOWER_BUILD_UI
-#include <G4UIExecutive.hh>
-#include <G4UImanager.hh>
-#include <G4VisExecutive.hh>
-#endif
-
-#ifdef G4MULTITHREADED
-#include <G4MTRunManager.hh>
-#else
 #include <G4RunManager.hh>
-#endif
 #include "configuration.h"
 #include "parameters.h"
 #include "recorder.h"
-#ifdef SHOWER_BENCHMARK
-#include "benchmark.h"
-#endif
 
 namespace Shower {
 class Core {
@@ -33,33 +20,12 @@ public:
     auto execute() -> int;
 
 private:
-#ifdef SHOWER_BUILD_UI
-    void setup(int argc, char* argv[]);
-#else
     void setup();
-#endif
 
-#ifdef SHOWER_BUILD_UI
-    auto execute_ui() -> int;
-#endif
-    auto execute_cli() -> int;
-
-#ifdef G4MULTITHREADED
-    G4MTRunManager* m_run_manager;
-#else
     G4RunManager* m_run_manager;
-#endif
 
-#ifdef SHOWER_BUILD_UI
-    G4VisManager* m_vis_manager;
-    G4UIExecutive* m_ui_executive;
-    G4UImanager* m_ui_manager;
-#endif
 
     std::shared_ptr<Configuration> m_configuration;
-#ifdef SHOWER_BENCHMARK
-    std::shared_ptr<Benchmark> m_benchmark;
-#endif
     std::shared_ptr<Parameters> m_parameters;
     std::shared_ptr<Recorder> m_recorder;
 };
--- source/src/core.cpp
+++ source/src/core.cpp
@@ -13,17 +13,8 @@ namespace Shower {
 Core::Core(int argc, char* argv[])
     : m_run_manager { nullptr }
     ,
-#ifdef SHOWER_BUILD_UI
-    m_vis_manager { nullptr }
-    , m_ui_executive { nullptr }
-    , m_ui_manager { nullptr }
-    ,
-#endif
     m_parameters { std::make_shared<Parameters>() }
 {
-#ifdef SHOWER_BUILD_UI
-    m_parameters->add_argument("g", "graphical", "Show the user interface");
-#endif
     m_parameters->add_argument("o", "overwrite", "Overwrite existing simulation data");
 
     m_parameters->add_argument("c", "config", "Use the configuration file specified in the value", true);
@@ -37,73 +28,24 @@ Core::Core(int argc, char* argv[])
     } else {
         m_configuration = std::make_shared<Configuration>();
     }
-#ifdef SHOWER_BENCHMARK
-    m_benchmark = std::make_shared<Benchmark>("benchmark");
-#endif
     m_recorder = std::make_shared<Recorder>(m_configuration, m_parameters);
-#ifdef SHOWER_BUILD_UI
-    setup(argc, argv);
-#else
     setup();
-#endif
 }
 
 Core::~Core()
 {
     delete m_run_manager;
-
-#ifdef SHOWER_BUILD_UI
-    delete m_vis_manager;
-
-    delete m_ui_executive;
-
-    delete m_ui_manager;
-
-#endif
 }
 
 auto Core::execute() -> int
 {
-
-#ifdef SHOWER_BUILD_UI
-    if (m_parameters->argument_set("g")) {
-        m_ui_manager = G4UImanager::GetUIpointer();
-
-        if (m_ui_executive != nullptr) {
-            return execute_ui();
-        }
-    }
-#endif
-    return execute_cli();
-}
-
-#ifdef SHOWER_BUILD_UI
-auto Core::execute_ui() -> int
-{
-    m_run_manager->Initialize();
-    m_ui_manager->ExecuteMacroFile("vis.mac");
-    m_ui_executive->SessionStart();
-    return 0;
-}
-#endif
-auto Core::execute_cli() -> int
-{
-    m_run_manager->BeamOn(m_configuration->get_events());
+    m_run_manager->BeamOn(1);
     return 0;
 }
 
-#ifdef SHOWER_BUILD_UI
-void Core::setup(int argc, char* argv[])
-#else
 void Core::setup()
-#endif
 {
     std::cout << "Starting simulation for '" << m_configuration->get_name() << "'\n";
-#ifdef SHOWER_BUILD_UI
-    if (m_parameters->argument_set("g")) {
-        m_ui_executive = new G4UIExecutive(argc, argv, "qt");
-    }
-#endif
     //    CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine {});
 
     std::random_device rd;
@@ -122,12 +64,7 @@ void Core::setup()
 
     CLHEP::HepRandom::setTheSeeds(seeds.data());
 
-#ifdef G4MULTITHREADED
-    m_run_manager = new G4MTRunManager {};
-    m_run_manager->SetNumberOfThreads(8);
-#else
     m_run_manager = new G4RunManager {};
-#endif
 
     m_run_manager->SetVerboseLevel(0);
 
@@ -137,20 +74,9 @@ void Core::setup()
 
     m_run_manager->SetUserInitialization(new PhysicsList { m_recorder, cancel_criterion});
 
-#ifdef SHOWER_BENCHMARK
-    m_run_manager->SetUserInitialization(new ActionInitialization { m_recorder, m_configuration, m_benchmark, cancel_criterion });
-#else
     m_run_manager->SetUserInitialization(new ActionInitialization { m_recorder, m_configuration, cancel_criterion });
-#endif
 
     m_run_manager->Initialize();
 
-#ifdef SHOWER_BUILD_UI
-    m_vis_manager = new G4VisExecutive {};
-
-    m_vis_manager->SetVerboseLevel(0);
-
-    m_vis_manager->Initialize();
-#endif
 }
 }
